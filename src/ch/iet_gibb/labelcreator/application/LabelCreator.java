1316d0cf21a4d3342524275978ac6bdac7fb96ca
Marjan Tomev <mto150238@stud.gibb.ch>
2024-08-15 18:22:32 +0200

    project example

---
+++ src/ch/iet_gibb/labelcreator/application/LabelCreator.java
@@ -0,0 +1,10 @@
+package ch.iet_gibb.labelcreator.application;
+
+public class LabelCreator {
+
+    public static void main(String[] args) {
+
+        // hier kommt der Startcode hinein, zum Test machen wir eine Terminalausgabe
+        System.out.println("Das ist die main-Methode");
+    }
+}
