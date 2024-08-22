616c1ce71547a04a0f7a86ed01c1c608d5dd940a
Marjan Tomev <mto150238@stud.gibb.ch>
2024-08-22 19:27:25 +0200

    feat: commented the ErrorMessage class

--- src/ch/iet_gibb/oecoheat/utils/ErrorMessage.java
+++ src/ch/iet_gibb/oecoheat/utils/ErrorMessage.java
@@ -1,5 +1,8 @@
 package ch.iet_gibb.oecoheat.utils;
 
+/**
+ * The ErrorMessage class provides error messages for the different classes in the application.
+ */
 public class ErrorMessage {
     public static final String volumeInLitersRange = "Volume in liters must be greater than zero.";
     public static final String radiusAndHeightRange = "Radius and height must be greater than zero.";
